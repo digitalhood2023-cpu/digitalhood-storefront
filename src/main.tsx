5284cd56cff431e151d13c0c3237acd05b475dc2
root <root@vmi2964183.contaboserver.net>
2026-05-11 23:46:38 +0200

    Update DigitalHood branding and loading experience

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+
 import App from './App';
-import ErrorBoundary from '@/components/ErrorBoundary';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ErrorBoundary>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ErrorBoundary>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
+
+window.addEventListener('load', () => {
+  setTimeout(() => {
+    const loader = document.getElementById('app-loader');
+
+    if (loader) {
+      loader.style.opacity = '0';
+      loader.style.transition = 'opacity 0.4s ease';
+
+      setTimeout(() => {
+        loader.remove();
+      }, 400);
+    }
+  }, 500);
+});
